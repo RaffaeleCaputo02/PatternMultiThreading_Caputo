23a3a3dcb0836a27088275e1120448a7854ec5ec
rcaputo95 <rcaputo95@gmail.com>
2020-04-26 03:05:03 +0200

    Aggiungere i file di progetto.

---
+++ Library Task/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Library_Task
+{
+    public class Worker
+    {
+        CancellationTokenSource _cts;
+        int _max;
+        int _delay;
+
+        public Worker(int max, int delay, CancellationTokenSource cts)
+        {
+            _max = max;
+            _delay = delay;
+            _cts = cts;
+        }
+            
+        public void Start()
+        {
+            Task.Factory.StartNew(DoWork);
+        }
+
+        private void DoWork()
+        {
+            for (int i = 0; i < _max; i++)
+            {
+                Thread.Sleep(_delay);
+                if (_cts.IsCancellationRequested)
+                    break;
+            }
+        }
+    }
+}
